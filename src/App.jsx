01d0ebd71994174cc6283ec4b02238318f284dcf
Atefe <aa.asgariyan12@gmail.com>
2023-11-30 18:21:19 +0330

    big screen why us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import AppLayout from "./components/AppLayout";
 import Home from "./pages/Home";
-import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import PageNotFound from "./pages/PageNotFound";
 import TeamCV from "./pages/TeamCV";
 import Services from "./pages/Services";
 import Process from "./pages/Process";
+import WhyUs from './pages/WhyUs';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
       <Route element={<AppLayout />}>
         <Route index element={<Navigate replace to="home" />} />
         <Route path="home" element={<Home />} />
-        <Route path="why-us" element={<AboutUs />} />
+        <Route path="why-us" element={<WhyUs />} />
         <Route path="contact" element={<ContactUs />} />
         <Route path="team-cv" element={<TeamCV />} />
         <Route path="services" element={<Services />} />
