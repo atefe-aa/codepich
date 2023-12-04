0c6b20557386aca8230def3eed0c3719846a2be2
Atefe <aa.asgariyan12@gmail.com>
2023-12-04 21:38:38 +0330

    create resume page

--- src/pages/AboutPerson.jsx
+++ src/pages/AboutPerson.jsx
@@ -1,49 +1,69 @@
+import { Link, Navigate, useParams } from "react-router-dom";
+import { teamMembersData } from "../utils/consts";
+import styles from "../styles/AboutPerson.module.css";
+
 function AboutPerson() {
+  const { name } = useParams();
+  const data = teamMembersData[name];
+
+  if (!data) return <Navigate to="/404" replace />;
+  
   return (
-    <section className="overflow-x-hidden bg-about-person">
-      <div className="person-ability container mt-5">
+    <section className={`overflow-x-hidden ${styles.bg_about_person}`}>
+      <div className={`container mt-5 ${styles.person_ability}`}>
         <div className="row">
           <div className="col-12 col-md-3 p-2 mb-4">
             <div className="rounded p-4">
               <div className="mb-4">
                 <div className="d-flex justify-content-center name">
-                  زهرا علی عسگریان
+                  {data.name}
                 </div>
-                <div className="job d-flex justify-content-center">گرافیست</div>
+                <div className="job d-flex justify-content-center">
+                  {data.jobTitle}
+                </div>
               </div>
             </div>
-            <div className="rounded p-4 mt-5 glass-bg-2">
-              <b>درباره من:</b>
-              <p className="my-3">
-                زهرا علی عسسگریا ن گرافیست زهرا علی عسسگریا ن گرافیست زهرا علی
-                عسسگریا ن گرافیست زهرا علی عسسگریا ن گرافیست زهرا علی عسسگریا ن
-                گرافیست
-              </p>
+            <div
+              className={`rounded p-4 mt-5 ${styles.glass_bg_2} ${styles.aboutDiv}`}
+            >
+              <div className={styles.personPicture}>
+                <img src={data.picture} alt={`${data.name}'s picture`} />
+              </div>
+              <div className="mt-5">
+                <b>درباره من:</b>
+                <p className="my-3">{data.about}</p>
+              </div>
             </div>
 
-            <div className="rounded-5 p-2 mt-4 glass-bg-2">
+            <div className={`rounded-5 p-2 mt-4  ${styles.glass_bg_2}`}>
               <img src="/assets/img/phone.svg" alt="phone" className="w-10" />
-              <span className="float-end">شماره تماس</span>
+              <span className="ms-2">{data.phone} </span>
             </div>
-            <div className="rounded-5 p-2 mt-4 glass-bg-2">
+            <div className={`rounded-5 p-2 mt-4  ${styles.glass_bg_2}`}>
               <img src="/assets/img/linkdin.svg" alt="phone" className="w-10" />
-              <span className="float-end">ادرس</span>
+              <Link to={data.linkedIn} className="ms-2">
+                لینکدین
+              </Link>
             </div>
-            <div className="rounded-5 p-2 mt-4 glass-bg-2">
+            <div className={`rounded-5 p-2 mt-4 ${styles.glass_bg_2}`}>
               <img
                 src="/assets/img/instagram.svg"
                 alt="phone"
                 className="w-10"
               />
-              <span className="float-end">آدرس</span>
+              <Link to={data.instagram} className="ms-2">
+                اینستاگرام
+              </Link>
             </div>
-            <div className="rounded-5 p-2 mt-4 glass-bg-2">
+            <div className={`rounded-5 p-2 mt-4  ${styles.glass_bg_2}`}>
               <img
                 src="/assets/img/telegram.svg"
                 alt="phone"
                 className="w-10"
               />
-              <span className="float-end">ادرس</span>
+              <Link to={data.telegram} className="ms-2">
+                تلگرام
+              </Link>
             </div>
           </div>
           <div className="col-12 col-md-9 p-2">
@@ -52,29 +72,118 @@ function AboutPerson() {
               style={{ background: "rgba(214, 214, 214, 0.95)" }}
             >
               <div className="education">
-                <h6 className="title p-2 rounded-3 d-inline ability-title">
-                  تحصیلات
-                </h6>
-                <div className="ability-person">تحصیلات وارد شود</div>
+                <div className={styles.titleBox}>
+                  <h6
+                    className={`p-2 rounded-3 d-inline ${styles.ability_title}`}
+                  >
+                    تحصیلات
+                  </h6>
+                  <div className={styles.hrLine}></div>
+                </div>
+                <div className={styles.ability_person}>
+                  {data.educations.map((edu) => (
+                    <div key={edu.title}>
+                      <h6>{edu.title}</h6>
+                      <p>{edu.description}</p>
+                    </div>
+                  ))}
+                </div>
               </div>
               <div className="sabeghe">
-                <h6 className="title-2 p-2 rounded-3 d-inline ability-title">
-                  سابقه کاری
-                </h6>
-                <div className="ability-person">سابقه کاری وارد شود</div>
+                <div className={styles.titleBox}>
+                  <h6
+                    className={`p-2 rounded-3 d-inline ${styles.ability_title}`}
+                  >
+                    سابقه کاری
+                  </h6>
+                  <div className={styles.hrLine}></div>
+                </div>
+                <div className={styles.ability_person}>
+                  {data.experiments.map((exp) => (
+                    <div key={exp.title}>
+                      <h6>{exp.title}</h6>
+                      <div>
+                        <span>{exp.toDate}</span>-<span>{exp.fromDate}</span>
+                      </div>
+                      <p>{exp.description}</p>
+                    </div>
+                  ))}
+                </div>
               </div>
               <div className="my-skill">
-                <h6 className="title-3 p-2 rounded-3 d-inline ability-title">
-                  مهارت های من
-                </h6>
-                <div className="ability-person">مهارت ها وارد شود</div>
+                <div className={styles.titleBox}>
+                  <h6
+                    className={`p-2 rounded-3 d-inline ${styles.ability_title}`}
+                  >
+                    مهارت های من
+                  </h6>
+                  <div className={styles.hrLine}></div>
+                </div>
+                <div className={`${styles.skills}`}>
+                  {data.skills.map((skill) => (
+                    <div className={styles.svgDiv} key={skill.title}>
+                      <svg className={styles.svg}>
+                        <circle
+                          cx="50"
+                          cy="50"
+                          r="40"
+                          stroke="#693194"
+                          strokeWidth="10"
+                          fill="none"
+                          strokeDasharray={(
+                            2 *
+                            Math.PI *
+                            40 *
+                            (skill.percentage / 100)
+                          ).toFixed(2)}
+                          transform="rotate(-90 50 50)" // Rotate the circle to start from the top (0 degrees)
+                          className={styles.bigCircle}
+                        />
+                        <circle
+                          cx="50"
+                          cy="30"
+                          r="25"
+                          stroke="#693194"
+                          strokeWidth="8"
+                          fill="none"
+                          strokeDasharray={(
+                            2 *
+                            Math.PI *
+                            30 *
+                            (skill.percentage / 100)
+                          ).toFixed(2)}
+                          transform="rotate(-90 50 50)" // Rotate the circle to start from the top (0 degrees)
+                          className={styles.smallCircle}
+                        />
+                        <image
+                          x="24%"
+                          y="25%"
+                          xlinkHref={skill.logo}
+                          className={styles.logo}
+                        />
+                      </svg>
+                    </div>
+                  ))}
+                </div>
               </div>
               <div className="alayegh">
-                <h6 className="title-4 p-2 rounded-3 d-inline ability-title">
-                  علایق من
-                </h6>
-                <div className="ability-person" style={{ minHeight: "200px" }}>
-                  وارد شود
+                <div className={styles.titleBox}>
+                  <h6
+                    className={`p-2 rounded-3 d-inline ${styles.ability_title}`}
+                  >
+                    علایق من
+                  </h6>
+                  <div className={styles.hrLine}></div>
+                </div>
+                <div
+                  className={styles.ability_person}
+                  style={{ minHeight: "200px" }}
+                >
+                  {data.interests.map((exp) => (
+                    <div key={exp}>
+                      <h5>{exp}</h5>
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,1 +1,157 @@
 export const BASE_URL = "https://bime.see5.net";
+
+export const teamMembersData = {
+  zahra: {
+    name: "زهرا",
+    jobTitle: "گرافیست",
+    picture: "/assets/img/members/zahra.jpg",
+    about:
+      "زهرا علی عسسگریا ن گرافیست زهرا علی عسسگریا ن گرافیست زهرا علی عسسگریا ن گرافیست زهرا علی عسسگریا ن گرافیست زهرا علی عسسگریا ن گرافیست ",
+    phone: "01234567891",
+    linkedIn: "https://www.linkedin.com/in/zahraid",
+    instagram: "https://www.instagram.com/zaraid",
+    telegram: "https://t.me/zahraid",
+    languages: [{ title: "انگلیسی", rating: 5 }] /** rating integer from 1-8 */,
+    educations: [
+      {
+        title: "کارشناسی",
+        description: "دانشگاه اصفهان از سال 1400",
+      },
+    ],
+    experiments: [
+      {
+        title: "گرافیست فریلنسر",
+        fromDate: "مهر1394",
+        toDate: "تیر1402",
+        description: "توضیحات در مورد نحوه کار و غیره",
+      },
+    ],
+    skills: [
+      {
+        title: "فتوشاپ",
+        logo: "/assets/img/skills/photoshop.svg",
+        percentage: 100,
+      },
+      {
+        title: "ایلاستریتور",
+        logo: "/assets/img/skills/illustrator.svg",
+        percentage: 80,
+      }, 
+       {
+        title: "افنرافکت",
+        logo: "/assets/img/skills/aftereffect.svg",
+        percentage: 50,
+      }, {
+        title: "پریمیر",
+        logo: "/assets/img/skills/premiere.svg",
+        percentage: 90,
+      },
+    ],
+    interests: ["موسیقی", "ورزش"],
+  },
+  atefe: {
+    name: "عاطفه",
+    jobTitle: "فول استک دولوپر",
+    picture: "/assets/img/members/zahra.png",
+    about:
+      "زهرا علی عسسگریا ن گرافیست زهرا علی عسسگریا ن گرافیست زهرا علی عسسگریا ن گرافیست زهرا علی عسسگریا ن گرافیست زهرا علی عسسگریا ن گرافیست ",
+    phone: "01234567891",
+    linkedIn: "https://www.linkedin.com/in/zahraid/",
+    instagram: "https://www.instagram.com/zaraid",
+    telegram: "https://t.me/zahraid",
+    languages: [{ title: "انگلیسی", rating: 8 },{ title: "آلمانی", rating: 2 }] /** rating integer from 1-8 */,
+    educations: [
+      {
+        title: "کارشناسی",
+        description: "دانشگاه صنعتی اصفهان از سال 1395 تا 1400",
+      },
+    ],
+    experiments: [
+      {
+        title: "بک اند فریلنسر",
+        fromDate: "مهر1394",
+        toDate: "تیر1402",
+        description: "توضیحات در مورد نحوه کار و غیره",
+      }, 
+    ],
+    skills: [
+      {
+        title: "php",
+        logo: "/assets/img/skills/php.svg",
+        percentage: 100,
+      },
+      {
+        title: "laravel",
+        logo: "/assets/img/skills/laravel.svg",
+        percentage: 80,
+      },
+    ],
+    interests: ["موسیقی", "ورزش"],
+  },
+  mahan: {
+    name: "ماهان",
+    jobTitle: "بک اند دولوپر",
+    picture: "",
+    about: "",
+    phone: "",
+    linkedIn: "",
+    instagram: "",
+    telegram: "",
+    languages: [{ title: "14774", rating: 8 }],
+    educations: [
+      {
+        title: "12",
+        description: "2422424",
+      },
+    ],
+    experiments: [
+      {
+        title: "2",
+        fromDate: "مهر1394",
+        toDate: "تیر1402",
+        description: "",
+      },
+    ],
+    skills: [
+      {
+        title: "155",
+        logo: "",
+        percentage: "",
+      },
+    ],
+    interests: ["686", "868"],
+  },
+  amir: {
+    name: "امیر",
+    jobTitle: "فرانت اند دولوپر",
+    picture: "",
+    about: "",
+    phone: "",
+    linkedIn: "",
+    instagram: "",
+    telegram: "",
+    languages: [{ title: "1", rating: 8 }],
+    educations: [
+      {
+        title: "1",
+        description: "",
+      },
+    ],
+    experiments: [
+      {
+        title: "1",
+        fromDate: "مهر1394",
+        toDate: "تیر1402",
+        description: "",
+      },
+    ],
+    skills: [
+      {
+        title: "1",
+        logo: "",
+        percentage: "",
+      },
+    ],
+    interests: ["", ""],
+  },
+};
